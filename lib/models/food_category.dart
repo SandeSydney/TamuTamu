5720832eda59b9925bb9b2c28202bebbf84e4aa7
SandeSydney <sandesydney01@gmail.com>
2020-12-07 12:39:18 +0300

    Creation of the food categories

---
+++ lib/models/food_category.dart
@@ -0,0 +1,13 @@
+/// FoodCategory class stores the available categories of food offered
+class FoodCategory {
+  final List _foodCategory = [
+    'breakfast',
+    'Lunch',
+    'Dinner',
+  ];
+
+  // getter method for the food category's length
+  void get foodCategorySize {
+    _foodCategory.length;
+  }
+}
