91a57709df6d456b49edda3ad08668e0ad0df4c6
SandeSydney <sandesydney01@gmail.com>
2020-12-08 16:46:39 +0300

    Added data and ways to access data

--- lib/models/food_category.dart
+++ lib/models/food_category.dart
@@ -1,13 +1,46 @@
+import 'dart:collection';
+
 /// FoodCategory class stores the available categories of food offered
 class FoodCategory {
-  final List _foodCategory = [
-    'breakfast',
-    'Lunch',
-    'Dinner',
+  // meals in the breakfast category
+  final List<String> _breakfastMeals = [
+    'Tea',
+    'Coffee',
+    'Porridge',
+    'Bread',
+    'Muffins',
   ];
 
-  // getter method for the food category's length
-  void get foodCategorySize {
-    _foodCategory.length;
+  // meals in the lunch category
+  final List<String> _lunchMeals = [
+    'Mokimo',
+    'Gidheri',
+    'Ugali',
+    'Kienyeji',
+    'Rice',
+  ];
+
+  // meals in the dinner category
+  final List<String> _dinnerMeals = [
+    'Pilau',
+    'Roast Beef',
+    'Eggs',
+    'Chicken stew',
+    'Pork',
+  ];
+
+  // display breakfast meals
+  UnmodifiableListView get breakfastMeals {
+    return UnmodifiableListView(_breakfastMeals);
+  }
+
+  // display lunch meals
+  UnmodifiableListView get lunchMeals {
+    return UnmodifiableListView(_lunchMeals);
+  }
+
+  // display supper meals
+  UnmodifiableListView get dinnerMeals {
+    return UnmodifiableListView(_dinnerMeals);
   }
 }
