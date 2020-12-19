b0eb7b85f4c591d114ec74f117854041620d3776
SandeSydney <sandesydney01@gmail.com>
2020-12-19 12:14:43 +0300

    Updated database helper

--- lib/util/dbhelper.dart
+++ lib/util/dbhelper.dart
@@ -17,27 +17,27 @@ class DbHelper {
         join(await getDatabasesPath(), 'tamutamu.db'),
         onCreate: (database, version) {
           database.execute(
-            'CREATE TABLE meal_category(category_id INTEGER PRIMARY KEY,category_name TEXT)',
+            'CREATE TABLE mealCategory(categoryId INTEGER PRIMARY KEY,categoryName TEXT)',
           );
           database.execute(
-            'CREATE TABLE meals(meal_id INTEGER PRIMARY KEY,category_id INTEGER,meal_name TEXT,meal_price TEXT,' +
-                'FOREIGN KEY(category_id) REFERENCES meal_category(category_id))',
+            'CREATE TABLE meals(mealId INTEGER PRIMARY KEY,categoryId INTEGER,mealName TEXT,mealPrice TEXT,' +
+                'FOREIGN KEY(categoryId) REFERENCES mealCategory(categoryId))',
           );
         },
         version: version,
       );
     }
-    return openDb();
+    return db;
   }
 
   // test the database
   Future testDb() async {
     db = await openDb();
     // populate the database
-    await db.execute('INSERT INTO meal_category VALUES(2, "Lunch")');
+    await db.execute('INSERT INTO mealCategory VALUES(2, "Lunch")');
     await db.execute('INSERT INTO meals VALUES(2, 2, "Ugali", "50 shillings")');
     // read from the database
-    List mealCategory = await db.rawQuery('SELECT * FROM meal_category');
+    List mealCategory = await db.rawQuery('SELECT * FROM mealCategory');
     List meals = await db.query('SELECT * FROM meals');
     // display results in the console
     print(mealCategory[0].toString());
@@ -47,7 +47,7 @@ class DbHelper {
   // insert data into MealCategory
   Future<int> insertMealCategory(MealCategory mealCategory) async {
     int categoryId = await this.db.insert(
-          'meal_category',
+          'mealCategory',
           mealCategory.toMap(),
           conflictAlgorithm: ConflictAlgorithm.replace,
         );
