a51499e0cde3bbe3ff9b34f8f9600dc25e89f180
SandeSydney <sandesydney01@gmail.com>
2020-12-19 10:31:27 +0300

    database helper class

--- lib/util/dbhelper.dart
+++ lib/util/dbhelper.dart
@@ -1,5 +1,7 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:tamutamu/models/meal_category.dart';
+import 'package:tamutamu/models/meals.dart';
 
 /// file contains the methods that are used for the database; CRUD
 class DbHelper {
@@ -15,23 +17,11 @@ class DbHelper {
         join(await getDatabasesPath(), 'tamutamu.db'),
         onCreate: (database, version) {
           database.execute(
-            '''
-            CREATE TABLE meal_category(
-              category_id INTEGER PRIMARY KEY,
-              category_name TEXT
-            )''',
+            'CREATE TABLE meal_category(category_id INTEGER PRIMARY KEY,category_name TEXT)',
           );
           database.execute(
-            '''
-            CREATE TABLE meals(
-              meal_id INTEGER PRIMARY KEY,
-              category_id INTEGER,
-              meal_name TEXT,
-              meal_price TEXT,
-                ''' +
-                '''
-              FOREIGN KEY(category_id) REFERENCES meal_category(category_id)
-            )''',
+            'CREATE TABLE meals(meal_id INTEGER PRIMARY KEY,category_id INTEGER,meal_name TEXT,meal_price TEXT,' +
+                'FOREIGN KEY(category_id) REFERENCES meal_category(category_id))',
           );
         },
         version: version,
@@ -44,13 +34,30 @@ class DbHelper {
   Future testDb() async {
     db = await openDb();
     // populate the database
-    await db.execute('INSERT INTO meal_category VALUES(1, "Breakfast")');
-    await db.execute('INSERT INTO meals VALUES(1, 1, "Tea", "50 shillings")');
+    await db.execute('INSERT INTO meal_category VALUES(2, "Lunch")');
+    await db.execute('INSERT INTO meals VALUES(2, 2, "Ugali", "50 shillings")');
     // read from the database
     List mealCategory = await db.rawQuery('SELECT * FROM meal_category');
     List meals = await db.query('SELECT * FROM meals');
     // display results in the console
     print(mealCategory[0].toString());
     print(meals[0].toString());
+  }
+
+  // insert data into MealCategory
+  Future<int> insertMealCategory(MealCategory mealCategory) async {
+    int categoryId = await this.db.insert(
+          'meal_category',
+          mealCategory.toMap(),
+          conflictAlgorithm: ConflictAlgorithm.replace,
+        );
+    return categoryId;
+  }
+
+  // insert item to Meals
+  Future<int> insertMeals(Meals meals) async {
+    int mealId = await this.db.insert('meals', meals.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+    return mealId;
   }
 }
